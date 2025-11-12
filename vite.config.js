7845335669e027ba1c73d0b995ed4b40f5ddc4a7
Vichi Troncoso Molina <vtroncosomolina@gmail.com>
2025-11-12 17:13:04 -0300

    feat: Configure project for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/pokedex/', // Adjust this to your repository name if different
   plugins: [
     vue(),
     vueDevTools(),
