f2e274a632f0357ea5127bffa02d96e41943748f
Vichi Troncoso Molina <vtroncosomolina@gmail.com>
2025-11-12 19:02:57 -0300

    remove dynamic import

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
 import { homeScrollY, lastFromRoute } from "@/composables/useScrollStore.js";
+import FavoritesView from "@/views/FavoritesView.vue";
+import HomeView from "@/views/HomeView.vue";
+import LoginView from "@/views/LoginView.vue";
+import PokemonDetailsView from "@/views/PokemonDetailsView.vue";
 import { createRouter, createWebHistory } from "vue-router";
+import NotFoundView from "../views/NotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,28 +12,28 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: () => import("../views/HomeView.vue"),
+      component: () => HomeView,
     },
     {
       path: "/login",
       name: "login",
-      component: () => import("../views/LoginView.vue"),
+      component: () => LoginView,
     },
     {
       path: "/favorites",
       name: "favorites",
-      component: () => import("../views/FavoritesView.vue"),
+      component: () => FavoritesView,
     },
     {
       path: "/pokemon/:id",
       name: "PokemonDetails",
-      component: () => import("../views/PokemonDetailsView.vue"),
+      component: () => PokemonDetailsView,
       props: true,
     },
     {
       path: "/:pathMatch(.*)*",
       name: "NotFound",
-      component: () => import("../views/NotFoundView.vue"),
+      component: () => NotFoundView,
     },
   ],
   scrollBehavior(to, from, savedPosition) {
