6aa4a0753a13d9285f479879f1bbf686e449a0d4
Vichi Troncoso Molina <vtroncosomolina@gmail.com>
2025-11-12 18:21:38 -0300

    chore: configs

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,15 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+import vueDevTools from "vite-plugin-vue-devtools";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/pokedex/', // Adjust this to your repository name if different
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: "/pokedex/",
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   build: {
@@ -21,8 +17,8 @@ export default defineConfig({
       output: {
         entryFileNames: `assets/[name]-[hash].js`,
         chunkFileNames: `assets/[name]-[hash].js`,
-        assetFileNames: `assets/[name]-[hash].[ext]`
-      }
-    }
-  }
-})
+        assetFileNames: `assets/[name]-[hash].[ext]`,
+      },
+    },
+  },
+});
